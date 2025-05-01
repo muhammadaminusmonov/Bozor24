f11b1e421b4055e60161da2a6379ff8029a8282f
as_abdik <asliddinabdiqodirov15@gmail.com>
2025-05-01 21:44:09 +0500

    initial commit

--- src/bozor/settings.py
+++ src/bozor/settings.py
@@ -128,6 +128,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
- 
 
-#hellooo
+
+a = 5
