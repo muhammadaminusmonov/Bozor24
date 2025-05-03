06a4acde3cd2d866c7521a462309d61ba390e1f0
asus185 <zetfliks@gmail.com>
2025-05-03 11:55:56 +0500

    attribute

---
+++ src/attribute/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Attribute(models.Model):
+    attribute_id = models.AutoField(primary_key=True)
+    category_id = models.foreignkey('Category', on_delete=models.CASCADE)
+    attribute_name = models.CharField(max_length=255)
+    unit = models.CharField(max_length=255)
+
+
+class product_attribute(models.Model):
+    product_attribute_id = models.AutoField(primary_key=True)
+    product_id = models.ForeignKey('Product', on_delete=models.CASCADE)
+    attribute_id = models.ForeignKey('Attribute', on_delete=models.CASCADE)
+    value = models.CharField(max_length=255)
+
