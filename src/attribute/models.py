ed2fee99221903078d21d357c5f910ecf16ebc48
asus185 <zetfliks@gmail.com>
2025-05-03 12:06:40 +0500

    attribute

--- src/attribute/models.py
+++ src/attribute/models.py
@@ -6,10 +6,15 @@ class Attribute(models.Model):
     attribute_name = models.CharField(max_length=255)
     unit = models.CharField(max_length=255)
 
+def __str__(self):
+    return self.attribute_name
 
 class product_attribute(models.Model):
     product_attribute_id = models.AutoField(primary_key=True)
     product_id = models.ForeignKey('Product', on_delete=models.CASCADE)
     attribute_id = models.ForeignKey('Attribute', on_delete=models.CASCADE)
     value = models.CharField(max_length=255)
+
+def __str__(self):
+    return self.value
 
