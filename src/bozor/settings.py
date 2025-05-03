94ce639f62cb95e2c653284bc7eb76f3c32fc843
muhammadaminusmonov <muhammadamincoder@gmail.com>
2025-05-03 15:33:01 +0500

    edited setting

--- src/bozor/settings.py
+++ src/bozor/settings.py
@@ -38,6 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'user',
+    'category',
+    'region',
+    'attribute',
+    'product',
+    'cart',
+    'order',
 ]
 
 MIDDLEWARE = [
