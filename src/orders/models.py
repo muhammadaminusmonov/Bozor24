8e6e4f3ab7e5a2162c935b0dfd1d67c83ec7b9f7
as_abdik <asliddinabdiqodirov15@gmail.com>
2025-05-03 13:09:04 +0500

    status faqat bosh

--- src/orders/models.py
+++ src/orders/models.py
@@ -4,19 +4,23 @@ from products.models import Product
 
 
 class Orders(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    total_price = models.BigIntegerField()
-    status = models.CharField(max_length=50)
-    payment_method = models.CharField(max_length=50)
-    customer_address = models.TextField()
+    STATUS_TYPES = [
+        (1, ""),
+    ]
+    PAYMENT_METHOD = [
+        (1, "Bozor24 account"),
+        (2, "Pay in received")
+    ]
+    status = models.CharField()
+    payment_method = models.CharField(choices=PAYMENT_METHOD)
+    customer_address = models.TextField(choices=STATUS_TYPES)
     is_paid = models.BooleanField(default=False)
     is_released = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.id} by {self.user.username}"
     
-class order_item(models.Model):
+class OrderItem(models.Model):
     order = models.ForeignKey(Orders, related_name='items', on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField()
