6d01b09ea7f65b8282f08408c98fc6fe06e436d5
TenWello <jahongirfayziyev94@gmail.com>
2025-05-04 17:56:33 +0500

    User va Payment moduli testingdan o'tdi va admin paneli qo'shildi

--- src/user/models.py
+++ src/user/models.py
@@ -3,12 +3,20 @@ from django.db import models
 from region.models import Region
 
 class User(AbstractUser):
-    status_type = []
-    role_type = []
+    status_type = [
+        (0, "Inactive"),
+        (1, "Active"),
+    ]
+
+    role_type = [
+        ("user", "User"),
+        ("admin", "Admin"),
+    ]
+
     total_money = models.BigIntegerField(default=0)
     status = models.SmallIntegerField(choices=status_type, default=0)
-    region = models.ForeignKey(Region, on_delete=models.PROTECT)
-    role = models.CharField(choices=role_type, default="user")
+    region = models.ForeignKey(Region, on_delete=models.SET_NULL, null=True, blank=True) # bu siz testcaselar ishlamayabdi
+    role = models.CharField(max_length=10, choices=role_type, default="user")
 
     def __str__(self):
         return self.username
