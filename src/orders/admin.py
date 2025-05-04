2c97da09c1b5aa51fb1a4dc155bdd5d8ff9edb3b
as_abdik <asliddinabdiqodirov15@gmail.com>
2025-05-04 17:09:30 +0500

    added admin, unittest for orders app

--- src/orders/admin.py
+++ src/orders/admin.py
@@ -1,5 +1,20 @@
 # orders/admin.py
 from django.contrib import admin
-from .models import Order
+from .models import Order, OrderItem
 
-admin.site.register(Order)
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('user', 'created_at', 'total_price', 'status', 'is_paid', 'is_released')
+    list_filter = ('status', 'payment_method', 'is_paid', 'is_released')
+    search_fields = ('user__username', 'customer_address')
+    inlines = [OrderItemInline]
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order', 'product', 'quantity', 'price_at_purchase')
+    search_fields = ('product__title',)
+
