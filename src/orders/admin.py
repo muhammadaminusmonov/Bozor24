6c580d1c4d7f637724dadfd2a5d5193df3810708
Azizbek <qodiraliyevazizbekpdp@gmail.com>
2025-05-04 15:03:08 +0500

    added admin.py and add admin.py, models.py tests

--- src/orders/admin.py
+++ src/orders/admin.py
@@ -1,13 +1,12 @@
-<<<<<<< HEAD
 # # orders/admin.py
 # from django.contrib import admin
 # from .models import Orders
 #
 # admin.site.register(Orders)
-=======
+
 # orders/admin.py
 from django.contrib import admin
 from .models import Order
 
 admin.site.register(Order)
->>>>>>> db895a93631b250cb8e265dead1175fe51df9a09
+
