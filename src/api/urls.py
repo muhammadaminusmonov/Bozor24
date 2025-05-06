2a5f749cb27c331ec8ae0c1c83bfcf906d519454
muhammadaminusmonov <muhammadamincoder@gmail.com>
2025-05-06 19:00:49 +0500

    payment & transaction start

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,13 +1,11 @@
-
-
 from django.urls import path, include
 from django.contrib import admin
 from django.urls import path, include
-
 
 
 urlpatterns = [
     path('product/', include('api.product.urls')),
     path("category/", include("api.category.urls")),
     path("auth/", include("api.auth.urls")),
+    path("payment/", include("api.payment.urls")),
 ]
