558748c6505454ffd04df24f222766b2b0926700
asus185 <zetfliks@gmail.com>
2025-05-06 18:49:45 +0500

    add_cart_api

--- src/api/urls.py
+++ src/api/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("category/", include("api.category.urls")),
-    path("auth/register/", include("api.auth.register.urls")),
-    path("auth/login/", include("api.auth.login.urls")),
-    path("auth/login_token/", include("api.auth.login_token.urls")),
+    path("auth/", include("api.auth.urls")),
+    path("cart/", include("api.cart.urls")),
 ]
