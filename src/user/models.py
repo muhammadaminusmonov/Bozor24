24fe93b0eaa952c1b2277c005c8d268d9ea74b88
muhammadaminusmonov <muhammadamincoder@gmail.com>
2025-05-06 13:58:58 +0500

    phone field added to user model

--- src/user/models.py
+++ src/user/models.py
@@ -20,6 +20,7 @@ class User(AbstractUser):
     status = models.SmallIntegerField(choices=status_type, default=0)
     region = models.ForeignKey(Region, on_delete=models.SET_NULL, null=True, blank=True)
     role = models.CharField(choices=role_type, default="user")
+    phone = models.CharField(max_length=15, null=True, blank=True)
 
     def __str__(self):
         return self.username
