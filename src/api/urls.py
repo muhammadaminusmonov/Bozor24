fd2bbad8787842c49529d104d5d0d362dc228aae
Azizbek <qodiraliyevazizbekpdp@gmail.com>
2025-05-07 18:48:26 +0500

    added promoted-product api

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,14 +1,9 @@
-
-
 from django.urls import path, include
-from django.contrib import admin
-from django.urls import path, include
-
-
 
 urlpatterns = [
     path('product/', include('api.product.urls')),
     path("category/", include("api.category.urls")),
     path("auth/", include("api.auth.urls")),
     path("region/", include("api.region.urls")),
+    path("promoted-product/", include("api.promoted-product.urls")),
 ]
