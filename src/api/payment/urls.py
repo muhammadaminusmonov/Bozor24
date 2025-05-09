1f94c72e8454fc8ffc8614389325f69dc26a969d
asus185 <zetfliks@gmail.com>
2025-05-09 19:33:08 +0500

    fix bag payment

--- src/api/payment/urls.py
+++ src/api/payment/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PaymentListCreateView, PaymentDetailView
+from .views import PaymentListCreateView, PaymentDetailView, PaymentCreateView
 
 urlpatterns = [
     path('', PaymentListCreateView.as_view(), name='payment_list_create'),
+    path('add/', PaymentCreateView.as_view(), name='payment_add'),  # to‘g‘ri View qo‘shildi
     path('<int:pk>/', PaymentDetailView.as_view(), name='payment_detail'),
 ]
--- src/comment/models.py
+++ src/comment/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from django.conf import settings
 from django.contrib.auth import get_user_model
 from product.models import Product
 
 User = get_user_model()
 
+# Asosiy abstract comment modeli
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField()
@@ -14,19 +14,16 @@ class Comment(models.Model):
     class Meta:
         abstract = True
 
-
+# Mahsulot uchun izohlar
 class CommentProduct(Comment):
-    product = models.ForeignKey(Product, related_name='comment_product', on_delete=models.CASCADE)  # Ideally should be a ForeignKey to a Product model
+    product = models.ForeignKey(Product, related_name='comment_product', on_delete=models.CASCADE)
 
     def __str__(self):
-        def __str__(self):
-            return f"Product Comment: {self.id}"
+        return f"Comment by {self.user.username} on {self.product.name}"
 
-
+# Foydalanuvchi (sotuvchi) uchun izohlar
 class CommentUser(models.Model):
-    seller = models.ForeignKey(User, related_name='user_comment', on_delete=models.CASCADE)  # Ideally should be a ForeignKey to a Seller model
+    seller = models.ForeignKey(User, related_name='user_comment', on_delete=models.CASCADE)
 
     def __str__(self):
-        def __str__(self):
-            return f"Product Comment: {self.id}"
-
+        return f"Comment on seller: {self.seller.username}"
