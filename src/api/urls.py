f93c355ce3177f693c77338379ae86a7995c65f0
asus185 <zetfliks@gmail.com>
2025-05-09 10:04:54 +0500

    add review

--- src/api/urls.py
+++ src/api/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path("auth/logout/", include("api.auth.logout.urls")),
     path("payment/", include("api.payment.urls")),
     path("cart/", include("api.cart.urls")),
-
+    path("review/", include("api.review.urls")),
     path("region/", include("api.region.urls")),
     path("promoted-product/", include("api.promoted-product.urls")),
     path("transaction/", include("api.transaction.urls")),
